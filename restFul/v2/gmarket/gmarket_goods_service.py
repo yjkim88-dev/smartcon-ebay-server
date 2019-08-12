cbee5209a7570acfb4f9594c5ad7b9d5e5d2e7db
cslee93 <cslee@smartcon-metaps.com>
2019-08-12 11:00:19 +0900

    [modify] error

--- restFul/v2/gmarket/gmarket_goods_service.py
+++ restFul/v2/gmarket/gmarket_goods_service.py
@@ -44,12 +44,12 @@ class GmarketGoodsService:
             try:
                 Logger.logger.info("==== SETEP1 AddItem Insert DB ====")
                 item_no = add_item_res_msg['GmktItemNo']
-                shipping_group_code = add_item_res_msg['ShippingGroupCode']
+                add_item_model.shipping['GroupCode'] = add_item_res_msg['ShippingGroupCode']
                 GoodsRegistDao().insertGoods(item_no, add_item_model, user_id)
 
-            except KeyError as key:
+            except BaseException as e:
                 Logger.logger.info("==== SETEP1 AddItem Insert DB Error====")
-                Logger.logger.info(key)
+                Logger.logger.info(e)
                 pass
 
         except BaseException as e:
